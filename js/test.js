b5475008782d26a96792e800e023f9e1ed530ca4
bart <bart@bart.com>
2018-01-27 22:10:31 +0100

    Second commit: button fix

--- js/test.js
+++ js/test.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
     var btn1 = document.getElementsByClassName('hello');
     btn1[0].addEventListener("click", function() {
-        alert('test');
+        alert('Hello World!');
     });
 
 };
