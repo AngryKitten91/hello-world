cae6f937a6399fdc1dc15b48e1cc6ff389ac5077
bart <bart@bart.com>
2018-01-28 16:08:29 +0100

    Third commit: Added jQuery, css and some cool stuff

--- js/test.js
+++ js/test.js
@@ -1,9 +1,26 @@
 
-window.onload = function() {
 
-    var btn1 = document.getElementsByClassName('hello');
-    btn1[0].addEventListener("click", function() {
-        alert('Hello World!');
+// OLD PURE JS BUTTON
+// window.onload = function() {
+
+//     var btn1 = document.getElementsByClassName('hello');
+//     btn1[0].addEventListener("click", function() {
+//         alert('Hello World!');
+//     });
+
+// };
+
+$(document).ready(function(){
+
+    //HELLO WORLD BUTTON
+    $('.hello').on('click', () => {
+        $('.content').fadeToggle('slow');
     });
 
-};
+    // THEME PICKER
+    $('.color-pick').on('click', 'span',function(){
+        var color = $(this).css('background-color');
+        $(':root').get(0).style.setProperty('--bg-color', color);
+    });
+
+});
